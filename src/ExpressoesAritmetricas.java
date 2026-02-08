a527be08b54a024469065bc2c6de0d637ad40172
Daniel Nazario <dmalzinha0221@gmail.com>
2026-02-08 16:01:37 -0400

    Expressões Aritmétricas  em Java

---
+++ src/ExpressoesAritmetricas.java
@@ -0,0 +1,13 @@
+public class ExpressoesAritmetricas {
+    // Expressões aritméticos
+    // + -> adição
+    // - -> subtração
+    // * -> multiplicação
+    // / -> divisão
+    // % -> resto da divisão
+
+    // Precedência
+    // () -> Alta prioridade
+    // 1° * / %
+    // 2° + -
+}
