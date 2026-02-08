42f5a190202beb4a8dd926033e7db9c738d93aaf
Daniel Nazario <dmalzinha0221@gmail.com>
2026-02-08 18:11:25 -0400

    Aprendendo diferenças de variaveis

---
+++ src/Variaval.java
@@ -0,0 +1,14 @@
+public class Variaval {
+    int idade = 25;
+    double altura = 1.68;
+    char sexo = 'F';
+    /*
+    Sintaxe:
+    <tipo> <nome> = <valor inicial>;
+    Uma variável possui:
+    - Nome (ou identificador)
+    - Tipo
+    - Valor
+    - Endereço
+    */
+}
