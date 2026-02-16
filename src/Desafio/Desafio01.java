427d839e0071ca1324014a6ceee10772e0a92fda
Daniel Nazario <dmalzinha0221@gmail.com>
2026-02-16 15:56:45 -0400

    Desafio

--- src/Desafio/Desafio01.java
+++ src/Desafio/Desafio01.java
@@ -1,4 +1,50 @@
 package Desafio;
 
 public class Desafio01 {
+    public static void main(String[] args) {
+        /* Crie um programa que represente tres ninjas da Vila da Folha (Konoha)
+        * de Naturo e suas respectativas missões. Cada ninja tem um nome, uma idade
+        * e uma missão atribuida a ele, com o nome da missão, nível de dificuldade
+        * e status de conclusao.
+        */
+
+        // Ninja 1
+        String nomeNinja1 = "Naruto";
+        int idadeNinja1 = 13;
+        String missaoNinja1 = "Encontrar o gato perdido";
+        String StatusDeConclusaodoNinja1 = "Em Andamento";
+        char NivelDeDificuldadedoNinja1 = 'D';
+
+        System.out.println("Nome do Ninja: " + nomeNinja1);
+        System.out.println("Idade do Ninja: " + idadeNinja1);
+        System.out.println("Missão do Ninja: " + missaoNinja1);
+        System.out.println("Status de Conclusao do Ninja: " + StatusDeConclusaodoNinja1);
+        System.out.println("Nivel de Dificuldade do Ninja: " + NivelDeDificuldadedoNinja1);
+        // Ninja 2
+        String nomeNinja2 = "Sakuke";
+        int idadeNinja2 = 14;
+        String missaoNinja2 = "Encontrar o Bandido perdido";
+        String StatusDeConclusaodoNinja2 = "Em Andamento";
+        char NivelDeDificuldadedoNinja2 = 'B';
+
+        System.out.println("Nome do Ninja: " + nomeNinja2);
+        System.out.println("Idade do Ninja: " + idadeNinja2);
+        System.out.println("Missão do Ninja: " + missaoNinja2);
+        System.out.println("Status de Conclusao do Ninja: " + StatusDeConclusaodoNinja2);
+        System.out.println("Nivel de Dificuldade do Ninja: " + NivelDeDificuldadedoNinja2);
+
+        // Ninja 3
+        String nomeNinja3 = "Sakura";
+        int idadeNinja3 = 15;
+        String missaoNinja3 = "Encontrar o Curar Aldeão";
+        String StatusDeConclusaodoNinja3 = "Concluida";
+        char NivelDeDificuldadedoNinja3 = 'E';
+
+        System.out.println("Nome do Ninja: " + nomeNinja3);
+        System.out.println("Idade do Ninja: " + idadeNinja3);
+        System.out.println("Missão do Ninja: " + missaoNinja3);
+        System.out.println("Status de Conclusao do Ninja: " + StatusDeConclusaodoNinja3);
+        System.out.println("Nivel de Dificuldade do Ninja: " + NivelDeDificuldadedoNinja3);
+
+    }
 }
