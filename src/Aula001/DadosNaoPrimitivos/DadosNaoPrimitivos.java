7e15198b2c797ffa3f284e43b77c31076388158f
Daniel Nazario <dmalzinha0221@gmail.com>
2026-02-16 15:12:28 -0400

    Desafio

--- src/Aula001/DadosNaoPrimitivos/DadosNaoPrimitivos.java
+++ src/Aula001/DadosNaoPrimitivos/DadosNaoPrimitivos.java
@@ -7,10 +7,15 @@ public class DadosNaoPrimitivos {
         * Dados não primitivos: String, Array, Class, enum
         * Objetivo: Criar um ninja, e atribuir a ele.
         */
+        // Declarando a variavel como NARUTO UZUMAKI
         String nome = "Naruto Uzumaki";
         String nomeUpperCase = nome.toUpperCase(); // ToUpperCase vai colocar tudo em CAPSLOK
         System.out.println("Esse texto esta em CAPSLOCK" + nomeUpperCase);
-        System.out.println(nome);
+        System.out.println("Esse texto está normal" + nome);
+
+        String aldeia = "ALDEIA DA FOLHA";
+        String aldeiaEmCaixaBaixa = aldeia.toLowerCase(); // toLowerCase vai colocar tudo normal
+        System.out.println("Esse texto está tudo em letra minuscula" + aldeiaEmCaixaBaixa);
 
     }
 }
