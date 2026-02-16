91b3d407734d8c4c966ec36e65c775ec37e43a38
Daniel Nazario <dmalzinha0221@gmail.com>
2026-02-16 11:54:25 -0400

    Tipos Nao Primitivos

---
+++ src/Aula001/DadosNaoPrimitivos/DadosNaoPrimitivos.java
@@ -0,0 +1,16 @@
+package Aula001.DadosNaoPrimitivos;
+
+public class DadosNaoPrimitivos {
+    static void main(String[] args) {
+        // Tudo que for digitado aqui dentro PSWA vai ser compilado java
+        /*
+        * Dados não primitivos: String, Array, Class, enum
+        * Objetivo: Criar um ninja, e atribuir a ele.
+        */
+        String nome = "Naruto Uzumaki";
+        String nomeUpperCase = nome.toUpperCase(); // ToUpperCase vai colocar tudo em CAPSLOK
+        System.out.println("Esse texto esta em CAPSLOCK" + nomeUpperCase);
+        System.out.println(nome);
+
+    }
+}
--- src/Aula001/DadosPrimitivos/DadosPrimitivos.java
+++ src/Aula001/DadosPrimitivos/DadosPrimitivos.java
@@ -2,11 +2,10 @@ package Aula001.DadosPrimitivos;
 
 import java.util.Locale;
 
-public class Dados {
+public class DadosPrimitivos {
     public static void main(String[] args) {
         // Processamento de dados em Java
         double x = 10.35784;
-        String nome = "Maria";
         int idade = 31;
         double renda = 4000.0;
         System.out.println(x);
@@ -16,6 +15,5 @@ public class Dados {
         System.out.printf("%.4f%n", x);
         System.out.println("RESULTADO = " + x + " METROS");
         System.out.printf("RESULTADO = %.2f metros%n", x);
-        System.out.printf("%s tem %d anos e granha R$ %.2f reais%n", nome, idade, renda);
     }
 }
